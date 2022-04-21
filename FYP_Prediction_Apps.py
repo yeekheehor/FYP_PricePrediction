20e0df579a7c9cbef2570d16d023ad3036a5d0ff
yeekheehor <104145696+yeekheehor@users.noreply.github.com>
2022-04-21 22:01:07 +0800

    Add files via upload

--- FYP_Prediction_Apps.py
+++ FYP_Prediction_Apps.py
@@ -81,10 +81,10 @@ def plot_bar(data, x, y, height,  margin, title_text=None):
 #----------------------------------------------------------------------------------------------------------------------------------
 # set page title
 st.set_page_config('Airbnb Price Prediction App')
-image = Image.open('D:\Academic\Airbnb_data\Airbnb-logo.jpg')
+image = Image.open('Airbnb-logo.jpg')
 st.image(image, width = 300)
 
-data = pd.read_csv("D:/Academic/Airbnb_data/listings_transformed_enc.zip")
+data = pd.read_csv("listings_transformed_enc.zip")
 
 X = data[['host_response_rate','host_acceptance_rate', 'bedrooms','bathroom', 'beds','accommodates','neighborhood_enc','room_type_enc','host_response_time_enc','host_is_superhost_enc','instant_bookable_enc','has_availability_enc']]
 Y = data['log_price']
@@ -96,7 +96,7 @@ menu = st.sidebar.selectbox("Menu", menu_list)
 
 if menu == 'Exploratory Data Analysis':
     st.title('Exploratory Data Analysis of Airbnb Properties Price for Different Cities')
-    data = pd.read_csv('D:/Academic/Airbnb_data/listings_new.zip')
+    data = pd.read_csv('listings_new.zip')
 
     st.header('Descriptive Analysis')
     st.table(summary_table(data))
